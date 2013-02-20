da69e3541a7f91586f2dd7a74d7f965be4f72c45
Mikes-dev <mikes.dev1@gmail.com>
2013-02-20 17:10:12 +1100

    Update part of CRUD

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -21,6 +21,22 @@ class ProjectsController < ApplicationController
 	def show
 		@Project = Project.find(params[:id])
 	end
+	
+	def edit
+		@Project = Project.find(params[:id])
+	end
+
+	def update
+		@Project = Project.find(params[:id])
+		if @Project.update_attributes(params[:project])
+			flash[:notice] = "Project has been updated."
+			redirect_to @Project
+		else
+			flash[:alert] = "Project has not been updated."
+			render :action => "edit"
+		end
+	end
+
 
 
 end
