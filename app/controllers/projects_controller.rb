e51f2a9aca05b9ae1e507012c72d61fb0e6ae916
Mikes-dev <mikes.dev1@gmail.com>
2013-03-20 15:14:33 +1100

    Don't show projects that a user doesn't have permission to see

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,12 +6,12 @@ class ProjectsController < ApplicationController
 										    :destroy]
 
 	before_filter :authorize_admin!, :except => [:index, :show]
-	before_filter :authenticate_user!, :only => [:show]
+	before_filter :authenticate_user!, :only => [:index, :show]
 	before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 
 
 	def index
-		@projects = Project.all
+		@projects = Project.for(current_user).all
 	end
 
 	def new
--- features/viewing_projects.feature
+++ features/viewing_projects.feature
@@ -8,6 +8,9 @@ Background:
 	| user@ticketee.com | password |
 	And I am signed in as them
 	And there is a project called "TextMate 2"
+	And there is a project called "Internet Explorer"
+	And I am on the homepage
+	Then I should not see "Internet Explorer"
 	And "user@ticketee.com" can view the "TextMate 2" project
 Scenario: Listing all projects
 	And I am on the homepage
