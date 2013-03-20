7d5a0ba4184c461f4e59ccfe78a0c36880fb9276
Mikes-dev <mikes.dev1@gmail.com>
2013-03-20 14:03:32 +1100

    Made projects only visible to users with permission to see them

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe ProjectsController do
 	let(:user) { create_user! }
-	end
 
 	let(:project) { FactoryGirl.create(:project) }
-
 
 	context "standard users" do
 		{ "new" => "get",
@@ -14,25 +12,29 @@ describe ProjectsController do
 			"update" => "put",
 			"destroy" => "delete" }.each do |action, method|
 				it "cannot access the #{action} action" do
-				sign_in(:user, user)
-				send(method, action.dup, :id => project.id)
-				response.should redirect_to(root_path)
-				flash[:alert].should eql("You must be an admin to do that.")
+					sign_in(:user, user)
+					send(method, action.dup, :id => project.id)
+					response.should redirect_to(root_path)
+					flash[:alert].should eql("You must be an admin to do that.")
+				end
 			end
-		end	
-	end
+		end
 
 
 
 
+			it "displays an error for a missing project" do
+  				sign_in(:user, user)
+  				get :show, :id => "not-here"
+  				response.should redirect_to(projects_path)
+  				message = "The project you were looking for could not be found."
+  				flash[:alert].should eql(message)
+			end
 
-
-
-	it "displays an error for a missing project" do
-  		get :show, :id => "not-here"
-  		response.should redirect_to(projects_path)
-  		message = "The project you were looking for could not be found."
-  		flash[:alert].should eql(message)
-	end
-
+			it "cannot access the show action" do
+				sign_in(:user, user)
+				get :show, :id => project.id
+				response.should redirect_to(projects_path)
+				flash[:alert].should eql("The project you were looking for could not be found.")
+			end
 end
