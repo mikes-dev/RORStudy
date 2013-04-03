f9c91c7e7b6473c24863b041ce0d93bb9817248f
Mikes-dev <mikes.dev1@gmail.com>
2013-04-03 17:16:38 +1100

    Users can leave comments on tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -31,6 +31,7 @@ class TicketsController < ApplicationController
 	end
 
 	def show
+		@comment = @ticket.comments.build
 	end
 
 	def edit
---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ActiveRecord::Base
+  attr_accessible :text, :ticket_id, :user_id, :user
+  validates :text, :presence => true
+  belongs_to :user
+end
